18ce6c114ee95240cdd210312b912c3c96254d86
Ken-zhin <kjsimeon.student@ua.edu.ph>
2025-08-15 14:23:32 +0800

    Updated workk

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
- 
+  
+ //*description below section "ClassNName"*//
 export default function Home() {
   return (
-    <Layout home>
+    <Layout home>                                              
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hello I am Ken-zhin J. Simeon, A software engineer and an student]</p>      /*description*/
+        <p>[Hello I am Ken-zhin J. Simeon, A software engineer and an student]</p>     
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
