cb79604cfe55c8a2e4ca32ea7e761c720e2b26c2
Ken-zhin <kjsimeon.student@ua.edu.ph>
2025-08-19 15:04:35 +0800

    first commit

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,10 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hello I am Ken-zhin J. Simeon, A software engineer and an student]</p>     
+        <p>Im a Ken-zhin J. Simeon, Live in San Jose city of San Fernando Pampanga,I am a student in UA studying Integration and architecture in 3rd year college,</p>
+        <p>My Hobbies are playing games, reading and wathcing, For the Favorite music is "Bury the Light" in Food is Adobo.</p>
+        <p>Seing myself in 5 years is pursuing innovation and create a piece of hardwork in web developer and sharing my skill to the younger ones.</p>
+
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
