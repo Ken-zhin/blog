a34022dd4723847367b18b4432eac7d043c78445
Ken-zhin <kjsimeon.student@ua.edu.ph>
2025-08-19 15:11:36 +0800

    first commit

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
  
-const name = 'Ken-zhin J. Simeon';                              /*Change name*/
+const name = 'Ken-zhin Simeon';                              /*Change name*/
 export const siteTitle = 'Next.js Sample Website';
  
 export default function Layout({ children, home }) {
